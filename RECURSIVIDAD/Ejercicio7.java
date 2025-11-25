5aab574b0f83cb12291671ad722cc294dc0773d4
MaricelMorales <moralesmaricel75@gmail.com>
2025-11-25 17:13:07 -0300

    Update Ejercicio7.java

--- RECURSIVIDAD/Ejercicio7.java
+++ RECURSIVIDAD/Ejercicio7.java
@@ -1,6 +1,4 @@
 package RECURSIVIDAD;
-/*7. Escribir un método recursivo int vocales(String cd) para calcular el número de vocales de una cadena. 
- */
 
 public class Ejercicio7 {
     public static int vocales(String cd, int i){
@@ -8,13 +6,13 @@ public class Ejercicio7 {
             return 0; 
         }
         
-        int suma = 0;
+        //int suma = 0;
         if (cd.charAt(i) == 'a' || cd.charAt(i) == 'e' || cd.charAt(i) == 'i' || cd.charAt(i) == 'o' || cd.charAt(i) == 'u' ||
             cd.charAt(i) == 'A' || cd.charAt(i) == 'E' || cd.charAt(i) == 'I' || cd.charAt(i) == 'O' || cd.charAt(i) == 'U') {
-            suma = 1;
+            //suma = 1;
+             return 1+ vocales(cd, i+1);
         }
-
-        return suma + vocales(cd, i+1);
+        //return suma + vocales(cd, i+1);     
 
     }
     public static void main(String[] args) {        
